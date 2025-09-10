42c62d77fa1b2d41a01bd12a2df68b77448b1266
KiraBegau <kira.begau@gmx.de>
2025-09-10 09:52:04 +0200

    Translate Every Word in Sentence

--- PigTranslator.js
+++ PigTranslator.js
@@ -1,9 +1,8 @@
 const args = process.argv.slice(2); // --> Nur die Argumente (Zahlen, Wörer,...) ohne den Pfad und den Speicherort)*//
 const Benutzereingabe = args[0];
 
-const word = Benutzereingabe;
-const firstLetter = word[0];
-const secondLetter = word[1];
+const sentence = args[0];
+const words = sentence.split(" ");
 
 function istKonsonant(letter) {
   const vowels = ["a", "e", "i", "o", "u"];
@@ -14,15 +13,20 @@ function istVokal(letter) {
   const vowels = ["a", "e", "i", "o", "u"];
   return vowels.includes(letter.toLowerCase());
 }
-if (istKonsonant(firstLetter) && istVokal(secondLetter)) {
-  const FallEins = word.slice(1) + firstLetter + "ay";
-  console.log("Pig Latin:", FallEins);
-} else if (istKonsonant(firstLetter) && istKonsonant(secondLetter)) {
-  const FallZwei = word.slice(2) + firstLetter + secondLetter + "ay";
-  console.log("New Word:", FallZwei);
-} else if (istVokal(firstLetter)) {
-  const FallDrei = word + "way";
-  console.log("New Word:", FallDrei);
-} else {
-  console.log("Bedingung nicht erfüllt"); //Eigentlich braucht man kein Else weil mit irgendeiner dieser Kombis muss das Wort ja anfangen)
-}
+
+const pigLatinWords = words.map((word) => {
+  const firstLetter = word[0];
+  const secondLetter = word[1];
+
+  if (istKonsonant(firstLetter) && istVokal(secondLetter)) {
+    return word.slice(1) + firstLetter + "ay";
+  } else if (istKonsonant(firstLetter) && istKonsonant(secondLetter)) {
+    return word.slice(2) + firstLetter + secondLetter + "ay";
+  } else if (istVokal(firstLetter)) {
+    return word + "way";
+  } else {
+    console.log("Bedingung nicht erfüllt"); //Eigentlich braucht man kein Else weil mit irgendeiner dieser Kombis muss das Wort ja anfangen)
+    return word;
+  }
+});
+console.log("You got:", pigLatinWords.join(" "));
